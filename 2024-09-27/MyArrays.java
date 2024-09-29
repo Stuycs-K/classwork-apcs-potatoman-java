8cc9553e8bbf10dfa04a95a65e19770dbacb8aa4
potatoman-java <nathanclam@icloud.com>
2024-09-29 13:13:05 -0400

    made first functions for both copy and concat

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -9,19 +9,39 @@ public class MyArrays {
     }
     return (str+"]");
   }
+  public static int[] returnCopy(int[]ary){
+    int[] copy = new int[ary.length];
+    for(int idx = 0; idx < ary.length; idx++){
+      copy[idx] = ary[idx];
+    }
+    return copy;
+  }
+  public static int[] concatArray(int[]ary1,int[]ary2){
+    int[] concat = new int[ary1.length+ary2.length];
+    int counter = 0;
+    for(int idx = 0; idx < ary1.length; idx++){
+      concat[counter]=ary1[idx];
+      counter ++;
+    }
+    for(int idx = 0; idx < ary2.length; idx++){
+      concat[counter]=ary2[idx];
+      counter++;
+    }
+    return concat;
+  }
   public static void main (String[] args) {
     System.out.println("Testing returnCopy");
     int[] testArray = new int[]{1,2,3,4};
-    System.out.println("expected value: "+ arrays.toString(testArray) + "returned value: "+ arrays.toString(returnCopy(testArray)));
-    if (Array.equals(testArray, returnCopy(testArray))){
+    System.out.println("expected value: "+ aryToString(testArray) + "returned value: "+ aryToString(returnCopy(testArray)));
+    if (Arrays.equals(testArray, returnCopy(testArray))){
       System.out.println("contents equal");
     }
     if(testArray == returnCopy(testArray)){
       System.out.println("same array");
     }
     int[] testArray1 = new int[]{5,2,10,13};
-    System.out.println("expected value: "+ arrays.toString(testArray1) + "returned value: "+ arrays.toString(returnCopy(testArray1)));
-    if (Array.equals(testArray1, returnCopy(testArray1))){
+    System.out.println("expected value: "+ aryToString(testArray1) + "returned value: "+ aryToString(returnCopy(testArray1)));
+    if (Arrays.equals(testArray1, returnCopy(testArray1))){
       System.out.println("contents equal");
     }
     if(testArray1 == returnCopy(testArray1)){
@@ -29,13 +49,13 @@ public class MyArrays {
     }
     System.out.println("Testing concatArray");
     int[] Arraycat = new int[]{1,2,3,4,5,2,10,13};
-    System.out.println("expected value: "+ arrays.toString(Arraycat) + "returned value: "+ arrays.toString(concatArray(testArray,testArray1)));
-    if (Array.equals(Arraycat, concatArray(testArray,testArray1))){
+    System.out.println("expected value: "+ aryToString(Arraycat) + "returned value: "+ aryToString(concatArray(testArray,testArray1)));
+    if (Arrays.equals(Arraycat, concatArray(testArray,testArray1))){
       System.out.println("contents equal");
     }
     int[] Arraycat1 = new int[]{5,2,10,13,1,2,3,4};
-    System.out.println("expected value: "+ arrays.toString(Arraycat1) + "returned value: "+ arrays.toString(concatArray(testArray1,testArray)));
-    if (Array.equals(Arraycat, concatArray(testArray,testArray))){
+    System.out.println("expected value: "+ aryToString(Arraycat1) + "returned value: "+ aryToString(concatArray(testArray1,testArray)));
+    if (Arrays.equals(Arraycat, concatArray(testArray,testArray))){
       System.out.println("contents equal");
     }
   }
