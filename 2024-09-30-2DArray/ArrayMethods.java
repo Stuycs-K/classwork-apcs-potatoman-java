f4d91fe4557df03ee13b866d3f676ab2a32cec68
potatoman-java <nathanclam@icloud.com>
2024-09-30 16:51:09 -0400

    added first command

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -10,10 +10,21 @@ public class ArrayMethods{
     }
     return (str+"]");
   }
+  public static String arrToString(int[][] nums){
+    String str = "[";
+    for (int idx = 0; idx < nums.length; idx++){
+      str+=arrToString(nums[idx]);
+      if (idx < nums.length - 1){
+        str += ", ";
+      }
+    }
+    return (str + "]")
+  }
   public static void main(String[] args){
+    //testing first function
     int[][] testArr= {{1,2,3},{0,0,0},{1,1,1,1,1}};
     System.out.println("Expected = " + "[[1, 2, 3], [0, 0, 0], [1, 1, 1, 1, 1]]" + "Result = " +arrToString(testArr));
     int[][] testArr1 = {{4,32,5,2},{1,6},{3,3,2}};
-    System.out.println("Expected = " + "")
+    System.out.println("Expected = " + "[[4, 32, 5, 2], [1, 6], [3, 3, 2]]" + "Result = " +arrToString(testArr1));
   }
 }
