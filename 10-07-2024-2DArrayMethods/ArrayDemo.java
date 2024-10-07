d52cdced2dbb3a26c5c23dfaaa5b03e23a179d01
potatoman-java <nathanclam@icloud.com>
2024-10-07 10:10:07 -0400

    added arr2DSum and test cases

--- 10-07-2024-2DArrayMethods/ArrayDemo.java
+++ 10-07-2024-2DArrayMethods/ArrayDemo.java
@@ -1,13 +1,19 @@
 import java.util.Arrays;
 public class ArrayDemo {
   public static void main(String[]args){
-    //testing countZeroes2D
     int[][] test1 = {{0,1,4},{0,14,0},{},{1,5,2}};
-    System.out.println("Expected = "+3+" Result = "+countZeroes2D(test1));
     int[][] test2 = {{},{}};
+    int[][] test3 = {{1,4,4},{9,1,5},{1,1,1,1,1,1,1}}
+    //testing countZeroes2D
+    System.out.println("Expected = "+3+" Result = "+countZeroes2D(test1));
     System.out.println("Expected = "+0+" Result = "+countZeroes2D(test2));
-    //testing
-  }
+    System.out.println("Expected = "+0+" Result = "+countZeroes2D(test3));
+    //testing arr2DSum
+    System.out.println("Expected = "+27+" Result = "+arr2DSum(test1));
+    System.out.println("Expected = "+0+" Result = "+ arr2DSum(test2));
+    System.out.println("Expected = "+31+" Result = "+ arr2DSum(test3));
+    //testing 
+}
   public static String arrToString(int[] nums){
     String str = "[";
     for (int idx = 0; idx < nums.length; idx++){
@@ -38,6 +44,15 @@ public class ArrayDemo {
       }
     }
     return counter;
+  }
+  public static int arr2DSum (int[][] nums){
+    int sum = 0;
+    for (int idx = 0; idx < nums.length; idx++){
+      for (int idx1 = 0; idx1 < nums[idx].length; idx1++){
+        sum += nums[idx][idx1];
+      }
+    }
+    return sum;
   }
 
 }
