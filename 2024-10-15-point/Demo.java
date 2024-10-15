8a7b4648b7bef141317f1f029463fd26001e49ec
potatoman-java <nathanclam@icloud.com>
2024-10-15 09:55:50 -0400

    added second distance method

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -20,9 +20,8 @@ public class Demo{
     System.out.println( p4);
     System.out.println( p5);
     System.out.println( distance(p1,p2));
-    /*
-    System.out.println( Point.distance(p1,p2));
+    //System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
-    */
+    
   }
 }
