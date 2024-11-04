ac1837956b01f1c3c509c3e69edd0ac3b731bf6b
Nathan Lam <nathanclam@icloud.com>
2024-11-04 18:18:58 -0500

    added countTrianglesB

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -31,8 +31,29 @@ public class TriangleTester {
       Scanner input = new Scanner(file);
       int t1s1, t1s2, t1s3, t2s1, t2s2, t2s3, t3s2, t3s3;
       while(input.hasNext()){
-        t1s1 = 
+        t1s1 = input.nextInt();
+        t2s1 = input.nextInt();
+        t3s1 = input.nextInt();
+        t1s2 = input.nextInt();
+        t2s2 = input.nextInt();
+        t3s2 = input.nextInt();
+        t1s3 = input.nextInt();
+        t2s3 = input.nextInt();
+        t3s3 = input.nextInt();
+        if(validTriangle(t1s1, t1s2, t1s3)){
+          counter++;
+        }
+        if(validTriangle(t2s1, t2s2, t2s3)){
+          counter++;
+        }
+        if(validTriangle(t3s1, t3s2, t3s3)){
+          counter++;
+        }
+        return(counter);
       }
+    }catch(FileNotFoundException ex){
+      System.out.println("File not found");
+      return 0;
     }
   }
   public static void main(String[] args){
