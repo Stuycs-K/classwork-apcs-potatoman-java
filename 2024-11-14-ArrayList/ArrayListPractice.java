cf1b992032cba6fc7e6a126b11e3afefda159cbe
potatoman-java <nathanclam@icloud.com>
2024-11-14 09:54:59 -0500

    made random ArrayList function

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
 public class ArrayListPractice{
-  
+  public static ArrayList<String> createRandomArray(int size){
+    ArrayList<String> randomList = new ArrayList<String>();
+    int randomInt;
+    while(size > 0){
+      randomInt= (int)(Math.random()*11);
+      if(randomInt == 0){
+        randomList.add("");
+      }
+      else{
+        randomList.add(randomInt+"");
+      }
+      size--;
+    }
+    return randomList;
+  }
 }
