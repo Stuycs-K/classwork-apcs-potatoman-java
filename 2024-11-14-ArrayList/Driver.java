7239c5b79304915b83501e639121d5a342423986
potatoman-java <nathanclam@icloud.com>
2024-11-14 09:59:58 -0500

    tested first function

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
+
 public class Driver{
-  
+  public static void main(String[] args){
+    System.out.println(ArrayListPractice.createRandomArray(2));
+    System.out.println(ArrayListPractice.createRandomArray(5));
+    System.out.println(ArrayListPractice.createRandomArray(0));
+    System.out.println(ArrayListPractice.createRandomArray(200000));
+
+
+  }
+
 }
