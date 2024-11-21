3b17ad8c437512e8fffe7795dabdc328a620d9bc
potatoman-java <nathanclam@icloud.com>
2024-11-21 09:49:49 -0500

    finished day1 part1

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -1,55 +1,57 @@
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 public class day1{
-  public static int findBlocks(String filename){
+  public static void findBlocks(String filename){
     try{
     File directions = new File(filename);
     Scanner input = new Scanner(directions);
     String temp;
     int direction = 90;
-    int verticalblocks = 0;
-    int horizontalblocks = 0;
+    int x = 0, y = 0;
+    int displacement;
+    int[] beentox = new int[100];
+    int[] beentoy = new int[100];
+    int counter = 0;
     while(input.hasNext()){
       temp = input.next();
-      temp = temp.substring(0, temp.length()-1);
-      if(temp.charAt(0)=='R'){
-        direction -= 90;
+      temp = temp.substring(0,temp.length()-1);
+      if (temp.charAt(0) == 'R'){
+        direction-=90;
       }
       else{
-        direction += 90;
+        direction+=90;
       }
-      if (direction < 0){
-        direction += 360;
+      if(direction >= 360){
+        direction-=360;
       }
-      if (direction > 360){
-        direction -= 360;
+      else if(direction < 0){
+        direction+=360;
       }
-      if (direction == 90){
-        verticalblocks+=Integer.parseInt(temp.substring(1));
+      displacement = Integer.parseInt(temp.substring(1));
+      if (direction == 180 || direction == 270){
+        displacement *= -1;
       }
-      else if (direction == 270){
-        verticalblocks-=Integer.parseInt(temp.substring(1));
+      if(direction == 0 || direction == 180){
+        x+=displacement;
       }
-      else if (direction == 0){
-        horizontalblocks+=Integer.parseInt(temp.substring(1));
+      else{
+        y+=displacement;
       }
-      else if (direction == 180){
-        horizontalblocks-=Integer.parseInt(temp.substring(1));
+      beentox[counter] = x;
+      beentoy[counter] = y;
+      for(int i = 0; i < counter; i++){
+        if (beentox[i]==x && beentoy[i]==y){
+          System.out.println(x+","+y);
+        }
       }
-      System.out.println(temp + " order");
-      System.out.println(direction + " facing");
-      System.out.println(Integer.parseInt(temp.substring(1)) + " distance");
-      System.out.println(verticalblocks + " vertical");
-      System.out.println(horizontalblocks + " horizontal");
     }
-    return horizontalblocks + verticalblocks;
   }catch(FileNotFoundException ex){
     System.out.println("File not found");
-    return 0;
   }
   }
   public static void main(String[] args){
-    System.out.println(findBlocks("day1input.txt"));
+    findBlocks("day1input.txt");
   }
 }
