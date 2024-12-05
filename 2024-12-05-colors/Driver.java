6c6532b506535d403de29230a7d973375a3bcd6e
potatoman-java <nathanclam@icloud.com>
2024-12-04 22:31:41 -0500

    finished HW

--- 2024-12-05-colors/Driver.java
+++ 2024-12-05-colors/Driver.java
@@ -23,7 +23,38 @@ public class Driver{
     System.out.print(Text.RESET);
     System.out.println("");
   }
+  public static int randomint(){
+    return((int)(Math.random()*99)+1);
+  }
+  public static void changeRightColor(int number){
+    if (number > 75){
+      Text.color(Text.GREEN, Text.BRIGHT);
+    }
+    if(number < 25){
+      Text.color(Text.RED, Text.BRIGHT);
+    }
+  }
   public static void main(String[] args){
-      border(80,30,41);
+    System.out.print(Text.HIDE_CURSOR);
+    System.out.print("\u001b[H");
+    border(80,30,Text.background(Text.RED));
+    int[] randarray = new int[3];
+    for(int i = 0; i<3; i++){
+      randarray[i]=randomint();
+    }
+    for(int i = 0; i<randarray.length; i++){
+      Text.go(2,20+20*i);
+      changeRightColor(randarray[i]);
+      System.out.print(randarray[i]);
+      System.out.print(Text.RESET);
+    }
+    Text.go(3,2);
+    System.out.print(Text.RESET);
+    Text.color(Text.background(Text.RED));
+    for(int i = 0; i<78; i++){
+      System.out.print(" ");
+    }
+    System.out.print(Text.RESET);
+    Text.go(31,1);
   }
 }
