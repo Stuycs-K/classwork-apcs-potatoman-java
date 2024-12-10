ab8bdd38bdd7a1b7b05561ffb68e94c920a8a313
Nathan Lam <nathanclam@icloud.com>
2024-12-09 20:54:28 -0500

    finished HW

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -2,9 +2,50 @@ import java.util.Scanner;
 public class Game{
   public static void main (String[]args){
     Scanner userInput = new Scanner(System.in);
-    Adventurer nerd = new CodeWarrior("josh", 20, "DrRacket");
-    System.out.println("What is your name?");
-    Adventurer You = new Necromancer(Scanner.nextLine(), 20);
-    System.out.println("You are a Necromancer named "+You.getName()+")
+    Adventurer nerd = new CodeWarrior("Jake", 20, "DrRacket");
+    System.out.println("Type your name:");
+    Adventurer You = new Necromancer(userInput.nextLine(), 20);
+    System.out.println("You are a Necromancer named "+You.getName()+".  Your opponent is a CodeWarrior named "+nerd.getName()+".");
+    System.out.println("BEGIN");
+    battlestats(You);
+    battlestats(nerd);
+    System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+    String input = userInput.nextLine();
+    int enemyattackchoice;
+    while(!input.equals("quit")){
+      if(input.equals("a")){
+        System.out.println(You.attack(nerd));
+      }else if(input.equals("sp")){
+        System.out.println(You.specialAttack(nerd));
+      }else if(input.equals("su")){
+        System.out.println(You.support());
+      }else if(! input.equals("quit")){
+        System.out.println("not a valid input.");
+      }
+      if((input.equals("a") || input.equals("sp") || input.equals("su")) && nerd.getHP()>0){
+        enemyattackchoice = (int) (Math.random()*3);
+        if (enemyattackchoice == 0){
+          nerd.attack(You);
+        }else if(enemyattackchoice == 1){
+          nerd.specialAttack(You);
+        }else if(enemyattackchoice == 2){
+          nerd.support();
+        }
+      }
+      battlestats(You);
+      battlestats(nerd);
+      if(You.getHP()<=0 || nerd.getHP()<=0) break;
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      input = userInput.nextLine();
+
+    }
+    if(You.getHP()<=0){
+      System.out.println("YOU LOSE");
+    }else if(nerd.getHP()<=0){
+      System.out.println("YOU WIN");
+    }
+  }
+  public static void battlestats(Adventurer P1){
+    System.out.println(P1.getName()+", "+P1.getHP()+"/"+P1.getmaxHP()+" HP, "+P1.getSpecial()+"/"+P1.getSpecialMax()+" "+P1.getSpecialName());
   }
 }
--- 2024-12-06-Adventurer/Necromancer.java
+++ 2024-12-06-Adventurer/Necromancer.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.ArrayList;
 public class Necromancer extends Adventurer{
-  private int souls = 0, maxSouls = 10, level = 0;
+  private int souls = 5, maxSouls = 10, level = 0;
   public Necromancer(String name){
     super(name);
   }
@@ -27,7 +27,7 @@ public class Necromancer extends Adventurer{
     int hit = (int) (damage.nextDouble()*(6));
     hit+=(5*level);
     other.applyDamage(hit);
-    return("You cast Hell Gate on " + other.getName() + " and dealt " + hit + " damage by briefly dropping them into hell.  You grabbed "+ this.restoreSpecial(hit) +" loose souls before the gate closed");
+    return("You cast Hell Gate on " + other.getName() + " and dealt " + (hit) + " damage by briefly dropping them into hell.  You grabbed "+ this.restoreSpecial(hit+1) +" loose souls before the gate closed");
   }
   public String support(Adventurer other){
     other.setHP(other.getHP()+5);
@@ -38,17 +38,19 @@ public class Necromancer extends Adventurer{
     return("you cast Bone Shield on yourself. Bones wrap around you and block the next 5 damage taken.");
   }
   public String specialAttack(Adventurer other){
+    int hit = (level+1)*10;
     if(souls >= 5){
-      other.applyDamage((level+1)*10);
+      other.applyDamage(hit);
       souls -=3;
-      return("you cast Soul Spasm.  You did "+(level+1)*10 + " damage by flinging some loose souls at them.  You managed to recapture 2 of them but the rest escaped.");
+      return("you cast Soul Spasm.  You did "+hit + " damage by flinging some loose souls at them.  You managed to recapture 2 of them but the rest escaped.");
     }else if(souls > 0){
-      return("you didn't have enough souls.  You decide to try anyway.  The souls consume a bit of your blood and explode in the enemy's face doing 5 damage to yourself and 15 damage to the opponent.");
-      this.applyDamage(5);
-      other.applyDamage(15);
+      this.applyDamage(hit);
+      other.applyDamage(hit);
+      this.setSpecial(0);
+      return("you didn't have enough souls.  You decide to try anyway.  The souls consume a part of your soul and fly at the enemy, you do "+hit+" damage, but you also take the same amount.  You lose all of your remaining souls.");
     }else{
-      return("you have no souls.  Wow, what a failure.  You weep in the corner solemly, dealing no damage. Fortunately, a soul takes pity on you and allows you to capture it");
       restoreSpecial(1);
+      return("you have no souls.  Wow, what a failure.  You weep in the corner solemly, dealing no damage. Fortunately, a soul takes pity on you and allows you to capture it");
     }
   }
 
